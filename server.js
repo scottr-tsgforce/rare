73c29c6e7b9633b1ddbb123ba06100fcc82bf271
Rasika Ekhalikar <rasika.r.ekhalikar@tsgforce.com>
2019-02-12 15:51:49 +0000

    added api to add user info

--- server.js
+++ server.js
@@ -23,9 +23,15 @@ app.get('/', function(req,res){
 	res.send('Hello world');
 });
 
-app.post('/findUserByLogin', function(req, res,next){
-    DB.findUserByLogin('test@testcom', function(err, result){
+app.post('/findUserByEmail', function(req, res,next){
+    DB.findUserByLogin(req.query.email, function(err, result){
     										res.status(200).json(result);
     } );
+});
+
+app.get('/addUsers', function(req, res){
+	console.log(req);
+    DB.registerUser(req.query.email, req.query.first_name, req.query.last_name);
+    
 });
 app.listen(3000);
